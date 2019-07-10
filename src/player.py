4e01fea41298847045eb48af6c73006e4233ae14
Chubbard022 <chubbard022@gmail.com>
2019-07-10 14:47:27 -0700

    adding items for the player

--- src/player.py
+++ src/player.py
@@ -4,9 +4,10 @@ from room import Room
 
 class Player(Room):
     health = 100
-    def __init__(self,name,current_location):
+    def __init__(self,name,current_location,items):
         self.name = name
         self.current_location = current_location
+        self.items = items
 
     def __str__(self):
         print(f"{self.name} is in the {self.current_location}")
