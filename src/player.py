55f3c3ea7ef09816a24fdcc953de9aa79dd7a839
Chubbard022 <chubbard022@gmail.com>
2019-07-11 08:27:46 -0700

    adding items to Player class

--- src/player.py
+++ src/player.py
@@ -1,14 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-from room import Room
 
-class Player(Room):
+class Player:
     health = 100
-    
-    def __init__(self,name,current_location,items):
+    def __init__(self,name,current_location):
         self.name = name
         self.current_location = current_location
-        self.items = items
+        self.items = []
 
-    def __str__(self):
-        print(f"{self.name} is in the {self.current_location}")
+    # def __str__(self):
+    #     print(f"{self.name} is in the {self.current_location}")
